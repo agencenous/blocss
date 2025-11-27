a6763a6d65ec6e94188163d90535ac32fa2a9724
Bastien Ho <bastien@avecnous.eu>
2025-11-27 16:39:05 +0100

    Allows to hide labels

--- src/components/style-control.js
+++ src/components/style-control.js
@@ -3,9 +3,10 @@ import {__} from '@wordpress/i18n';
 import ColorControl from './color-control.js';
 import SizeControl from './size-control.js';
 import FontControl from "./font-control.js";
+import ControlLabel from "./control-label.js";
 
 const StyleControl = ( props ) => {
-    const { onChange, value = {}, font, color } = props;
+    const { onChange, value = {}, font, color, labels=true } = props;
 
     const { backgroundColor, color: textColor } = value;
 
@@ -13,19 +14,17 @@ const StyleControl = ( props ) => {
       <fieldset className="blocss-style-control">
         {font && FontControl && (
           <div>
-            <h4>{__("Typography")}</h4>
+            {labels && <ControlLabel label={__("Typography")} />}
             <FontControl
               value={value}
-              onChange={(newValue) =>
-                onChange({ ...value, ...newValue })
-              }
+              onChange={(newValue) => onChange({ ...value, ...newValue })}
               font={font}
             />
           </div>
         )}
         {((color && color.text) || (color && color.background)) && (
           <div>
-            <h4>{__("Colors")}</h4>
+            {labels && <ControlLabel label={__("Colors")} />}
             {ColorControl && color && color.background && (
               <ColorControl
                 label={__("Background Color")}
