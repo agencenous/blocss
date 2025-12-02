191163da2e0c324504c82230f6ccd7e7c8cbe6f5
Bastien Ho <bastien@avecnous.eu>
2025-12-02 15:39:39 +0100

    Support native WP vars as value

--- blocss.php
+++ blocss.php
@@ -1,6 +1,18 @@
 <?php
 namespace Blocss;
 
+function transform_wp_vars($value){
+    if (is_array($value)) {
+        foreach ($value as $key => $val) {
+            $value[$key] = transform_wp_vars($val);
+        }
+    } elseif (is_string($value) && preg_match('/var\:/', $value)) {
+        $value = str_replace('|', '--', $value);
+        $value = str_replace('var:', 'var(--wp--', $value) . ')';
+    }
+    return $value;
+}
+
 function generate_css($css_rules, $prefix_selector){
     $styles = '';
     foreach ($css_rules as $sub_selector => $rules) {
@@ -9,6 +21,8 @@ function generate_css($css_rules, $prefix_selector){
             if ($value === null) {
                 continue;
             }
+            $value = transform_wp_vars($value);
+            $value = is_array($value) ? implode(' ', $value) : $value;
             $dash = str_replace( '_', '-', preg_replace( '/([a-z])([A-Z])/', '$1-$2', $property ) );
             $styles .= sprintf('%s: %s;', $dash, $value);
         }
